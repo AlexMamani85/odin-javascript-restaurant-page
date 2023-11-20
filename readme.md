0376f2ced374da30050b294bec996dd0b5cd3f7f
AlexMamani85 <alex.mamani@tecsup.edu.pe>
2023-11-19 21:39:04 -0500

    ADD: development + html-webpack-plugin

--- readme.md
+++ readme.md
@@ -4,4 +4,4 @@ npm install webpack webpack-cli --save-dev
 
 npm install
 
-4.  Create a bare-bones homepage for a restaurant. Include an image, headline, and some copy about how wonderful the restaurant is. It’s okay to hard-code these into the HTML for now just to see how they look on the page.
+npm install --save-dev html-webpack-plugin
--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,16 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 const path = require('path');
 
 module.exports = {
+  mode: 'development',
   entry: './src/index.js',
+  devtool: 'inline-source-map',
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: 'Development',
+      template: './src/index.html',
+    }),
+  ],
   output: {
     filename: 'main.js',
     path: path.resolve(__dirname, 'dist'),
